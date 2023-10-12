c41d5b2d6fb72e870867294ff26de76186df09ce
mwdossantos <matthew.martijn.wilson@gmail.com>
2023-10-12 10:20:08 +0200

    fix

--- web/src/components/space.tsx
+++ web/src/components/space.tsx
@@ -1,17 +1,25 @@
-export function Space({ title, people, available }: { title: string, people: string, available: boolean }) {
+export function Space({ title, people, available, secondaryDescription, time }: { time?: string, secondaryDescription?: string, title: string, people?: string, available: boolean }) {
     return (
         <div className='grid grid-cols-[auto_1fr_auto] gap-4 items-center bg-grey '>
             <div>
-                <img className='w-[50px] aspect-square' src={`/${title}.svg`} alt={`Icon for ${title}`} />
+                <img className='w-[50px] aspect-square' src={`/${secondaryDescription ? secondaryDescription : title}.svg`} alt={`Icon for ${title}`} />
             </div>
             <div className="flex flex-col items-start">
                 <p className='text-blue'>{title}</p>
-                <p className="text-blue text-xs">{people} personen</p>
+                {secondaryDescription ? <p className="text-blue text-xs">{secondaryDescription}</p> : null}
+                {!secondaryDescription ? <p className="text-blue text-xs">{people} personen</p> : null}
             </div>
-            <div className='flex items-center gap-1 mr-4'>
-                <img src={available ? `/available.svg` : `/not-available.svg`} alt={available ? `Available` : `Not available`} className="w-4 h-4" />
-                <p className={available ? `text-green` : `text-red`}>{available ? `Beschikbaar` : `Bezet`}</p>
-            </div>
+            {time ? <p className="text-blue text-xs mr-4">{time}</p> : null}
+
+            {!time ?
+
+                <div className='flex items-center gap-1 mr-4'>
+                    <img src={available ? `/available.svg` : `/not-available.svg`} alt={available ? `Available` : `Not available`} className="w-4 h-4" />
+                    <p className={available ? `text-green` : `text-red`}>{available ? `Beschikbaar` : `Bezet`}</p>
+                </div>
+                :
+                null
+            }
         </div>
     )
 }
--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -18,11 +18,9 @@ export default function Dashboard() {
                 <section className='page-padding flex flex-col gap-4'>
                     <p className='text-blue text-2xl'>Dagprogramma</p>
                     <div className='flex flex-col gap-4'>
-                        <Space title="Entree" people="2 - 12" available />
-                        <Space title="Ontmoetplein" people="2 - 12" available />
-                        <Space title="Atelier" people="2 - 12" available={false} />
-                        <Space title="Landschapszaal" people="2 - 12" available={false} />
-                        <Space title="Co-creatie" people="2 - 12" available />
+                        <Space title="Teamoverleg ruimtelijke puzzel" time="Nu bezig" secondaryDescription="Atelier" available={false} />
+                        <Space title="Startup your day right - koffiemoment" time="9:30 - 10:00" secondaryDescription="Landschapszaal" available={false} />
+                        <Space title="Workshop digitalisatie" time="13:30 - 14:30" secondaryDescription="Co-creatie" available />
                     </div>
                     <div className="grid grid-cols-2 ">
                         <div className="p-4 bg-blue text-white flex flex-col gap-4 justify-center h-full">
