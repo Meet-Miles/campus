4c0c8e7c8379dd8580dd3dc5f1baeaac72b0603a
mwdossantos <matthew.martijn.wilson@gmail.com>
2023-12-15 22:47:12 +0100

    fix draw

--- web/src/components/editor.tsx
+++ web/src/components/editor.tsx
@@ -4,7 +4,7 @@ import '@tldraw/tldraw/tldraw.css'
 
 export default function Editor() {
     return (
-        <div className="tldraw__editor w-full" style={{ height: `calc(100vh - 152px)` }}>
+        <div className="tldraw__editor w-full " style={{ height: `calc(100svh - 152px)` }}>
             <Tldraw />
         </div>
     )
